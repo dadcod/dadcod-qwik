7533cf1098a3d56beb5cf988a19245e286d79787
dadcod <dadcod.blog@gmail.com>
2023-12-06 20:47:37 +0200

    Add styles to code in mdx. Add header animation and default styles.

--- src/components/starter/header/header.tsx
+++ src/components/starter/header/header.tsx
@@ -1,41 +1,47 @@
-import { component$ } from "@builder.io/qwik";
-import { QwikLogo } from "../icons/qwik";
-import styles from "./header.module.css";
+import Logo from '~/media/rope.webp?jsx';
+import { component$ } from '@builder.io/qwik';
+import styles from './header.module.css';
 
 export default component$(() => {
+  const sections = [
+    {
+      name: 'Home',
+      id: 'section-home',
+    },
+    {
+      name: 'Portfolio',
+      id: 'section-portfolio',
+    },
+    {
+      name: 'Contact',
+      id: 'section-contact',
+    },
+  ];
+
   return (
     <header class={styles.header}>
-      <div class={["container", styles.wrapper]}>
+      <div class={['container', styles.wrapper]}>
         <div class={styles.logo}>
           <a href="/" title="qwik">
-            <QwikLogo height={50} width={143} />
+            <Logo style={{ width: 100, height: 100 }} />
           </a>
         </div>
         <ul>
-          <li>
-            <a
-              href="https://qwik.builder.io/docs/components/overview/"
-              target="_blank"
-            >
-              Docs
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://qwik.builder.io/examples/introduction/hello-world/"
-              target="_blank"
-            >
-              Examples
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://qwik.builder.io/tutorial/welcome/overview/"
-              target="_blank"
-            >
-              Tutorials
-            </a>
-          </li>
+          {sections.map((section) => (
+            <>
+              <li>
+                <a href={`#${section.id}`}>
+                  <span class="animated-button">
+                    <span>
+                      {section.name.split('').map((letter,index) => (
+                        <em key={index}>{letter}</em>
+                      ))}
+                    </span>
+                  </span>
+                </a>
+              </li>
+            </>
+          ))}
         </ul>
       </div>
     </header>
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,60 @@ import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(() => {
+import { getHighlighter, type Highlighter } from 'shiki';
+
+let highlighter: Highlighter;
+
+export default defineConfig(async () => {
+  const { default: rehypePrettyCode } = await import('rehype-pretty-code');
+
+
+  async function getOrCreateHighlighter() {
+    if (highlighter) {
+      return highlighter;
+    }
+    highlighter = await getHighlighter({ theme: 'monokai' });
+    return highlighter;
+  }
+
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [ qwikCity({
+        mdxPlugins: {
+          rehypeSyntaxHighlight: false,
+          remarkGfm: true,
+          rehypeAutolinkHeadings: true,
+        },
+        mdx: {
+          rehypePlugins: [
+            
+            [
+              () => rehypePrettyCode({theme: 'monokai', keepBackground:false}) as any,
+              {
+                getHighlighter: getOrCreateHighlighter,
+                onVisitLine(node: any) {
+                  // Prevent lines from collapsing in `display: grid` mode, and allow empty
+                  // lines to be copy/pasted
+                  if (node.children.length === 0) {
+                    node.children = [{ type: 'text', value: ' ' }];
+                  }
+                },
+                onVisitHighlightedLine(node: any) {
+                  // Each line node by default has `class="line"`.
+                  if (node.properties.className) {
+                    node.properties.className.push('line--highlighted');
+                  }
+                },
+                onVisitHighlightedWord(node: any) {
+                  if (node.properties.className) {
+                    node.properties.className = ['word--highlighted'];
+                  }
+                },
+              },
+            ],
+            
+          ],
+        },
+      }), qwikVite(), tsconfigPaths()],
     dev: {
       headers: {
         "Cache-Control": "public, max-age=0",

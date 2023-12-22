0d6ea679f7e0dc5f2b897a2c6468a7495ef93427
dadcod <dadcod.blog@gmail.com>
2023-12-22 18:51:55 +0200

    loading of posts

---
+++ src/utils/posts.ts
@@ -0,0 +1,151 @@
+import fs from 'fs';
+import matter from 'gray-matter';
+
+const BLOG_DIR = 'src/routes/blog';
+
+const load = () => {
+  const files = fs.readdirSync(BLOG_DIR, { recursive: true });
+  const posts = Promise.all(
+    files
+      .filter((filename) => filename.endsWith('.mdx'))
+      .map(async (filename) => {
+        const slug = filename.replace('.mdx', '');
+        return await findPostBySlug(slug);
+      })
+  );
+
+  return posts;
+};
+export interface Post {
+  /** A unique ID number that identifies a post. */
+  id: string;
+
+  /** A post’s unique slug – part of the post’s URL based on its name, i.e. a post called “My Sample Page” has a slug “my-sample-page”. */
+  slug: string;
+
+  // /**  */
+  // permalink: string;
+
+  /**  */
+  publishDate: Date;
+  /**  */
+  updateDate?: Date;
+
+  /**  */
+  title: string;
+  /** Optional summary of post content. */
+  excerpt?: string;
+  /**  */
+  image?: string;
+
+  /**  */
+  category?: string;
+  /**  */
+  tags?: Array<string>;
+  /**  */
+  author?: string;
+
+  /**  */
+  metadata?: any;
+
+  /**  */
+  draft?: boolean;
+
+  /**  */
+  readingTime?: number;
+}
+
+let _posts: Post[];
+
+// Ensure only the minimal needed data is exposed
+// fields.forEach((field) => {
+//   if (field === 'slug') {
+//     items[field] = realSlug
+//   }
+//   if (field === 'content') {
+//     items[field] = content
+//   }
+
+//   if (typeof data[field] !== 'undefined') {
+//     items[field] = data[field]
+//   }
+// })
+
+/** */
+export const fetchPosts = async (): Promise<Post[]> => {
+  _posts = _posts || load();
+
+  return await _posts;
+};
+
+/** */
+export const findLatestPosts = async ({ count, page }: { count?: number; page?: number } = {}): Promise<Post[]> => {
+  const _count = count || 4;
+  const _page = page || 1;
+  const posts = await fetchPosts();
+
+  return posts ? posts.slice((_page - 1) * _count, (_page - 1) * _count + _count) : [];
+};
+
+/** */
+export const findPostBySlug = async (slug: string): Promise<Post | null> => {
+  if (!slug) return null;
+
+  try {
+    const readFile = fs.readFileSync(BLOG_DIR + `/${slug}.mdx`, 'utf-8');
+    const { data } = matter(readFile);
+    const {
+      publishDate: rawPublishDate = new Date(),
+      updateDate: rawUpdateDate,
+      title,
+      excerpt,
+      image,
+      tags = [],
+      category,
+      author,
+      draft = false,
+      metadata = {},
+    } = data;
+
+    const publishDate = new Date(rawPublishDate);
+    const updateDate = rawUpdateDate ? new Date(rawUpdateDate) : undefined;
+
+    return {
+      id: slug,
+      slug: slug,
+
+      publishDate: publishDate,
+      updateDate: updateDate,
+
+      title: title,
+      excerpt: excerpt,
+      image: image,
+
+      category: category,
+      tags: tags,
+      author: author,
+
+      draft: draft,
+
+      metadata,
+    };
+  } catch (e) {
+    /* empty */
+  }
+
+  return null;
+};
+
+/** */
+export const findPostsByIds = async (ids: string[]) => {
+  if (!Array.isArray(ids)) return [];
+
+  const posts = await fetchPosts();
+
+  return ids.reduce(function (r: Post[], id: string) {
+    posts.some(function (post: Post) {
+      return id === post.id && r.push(post);
+    });
+    return r;
+  }, []);
+};

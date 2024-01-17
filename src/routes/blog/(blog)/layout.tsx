8cf183a71ad3d0997b47909d1683bbd75555062b
dadcod <dadcod.blog@gmail.com>
2024-01-17 20:57:57 +0200

    fix layout

--- src/routes/blog/(blog)/layout.tsx
+++ src/routes/blog/(blog)/layout.tsx
@@ -2,7 +2,7 @@ import { Slot, component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return (
-    <div class="max-w-screen-md mx-auto">
+    <div class="max-w-screen-md mx-auto w-full">
       <Slot />
     </div>
   );
